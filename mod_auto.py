9acb4af89b6d1887826a6d325f53f411eb79c6c6
Michael <michaealcapone21@gmail.com>
2016-03-22 21:57:05 +0000

    new version works with nettables

--- mod_auto.py
+++ mod_auto.py
@@ -3,15 +3,16 @@ from kivy.config import Config
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.image import Image
-import joystick_protocol
-import encode
+import serial
+import time
 
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '480')
 
 class MyPaintApp(App):
 	
-	defenses = ('Portcullis','Cheval\nde Frise','Moat','Ramparts','Drawbridge', 'Sally Port','Rock Wall', 'Rough Terrain')
+	port= None
+	defenses = ('Portcullis','Frise','Moat','Ramparts','Drawbridge', 'SallyPort','RockWall', 'RoughTerrain')
 	abvr_defenses = ['po', 'fr', 'mo', 'ra', 'br','sa', 'rk', 'ro'] 
 	robot = ['', 'Robot']
 	robot_return = ['', 'Return']
@@ -23,7 +24,7 @@ class MyPaintApp(App):
 	button_index=[0,0,0,0]
 	position_index = [0,0,0,0,0]
 	position_index2 = [0,0,0,0,0]
-	high_low_index = ['High', 'Low']
+	high_low_index = ['High shot', 'No Shot']
 	spy_index=['Spy\n Position\n on','Spy\n Position\n off']
 	return_index=['Return On', 'Return Off']
 	send_list=['lb', '', '', '', '','','']
@@ -35,10 +36,12 @@ class MyPaintApp(App):
 	spy_button_index=0
 	#Puts together the window, and the widgets in it
 	def build(self):
+		self.port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 		parent = Image(source ='redside480.png')
 		
 		self.defense_button1 = Button(text='Low Bar', pos=(480, 28), size=(100, 75))
-		
+		self.port.write("defense1 LowBar:")
+
 		self.defense_button2 = Button(text= self.defenses[0], pos=(480, 95), size=(100, 75))
 		self.defense_button2.bind(on_release=self.defense_button2_callback)
 
@@ -54,19 +57,19 @@ class MyPaintApp(App):
 		self.config_button = Button(text='Config Mode', pos=(0,300), size=(100, 100)) 
 		self.config_button.bind(on_release=self.config_button_callback)
 		
-		self.position_button1 = Button(text='', pos=(578,95), size=(100, 75)) 
+		self.position_button1 = Button(text='', pos=(578,28), size=(100, 75)) 
 		self.position_button1.bind(on_release=self.position_button1_callback)
 		
-		self.position_button2 = Button(text=' ', pos=(578,168), size=(100, 75)) 
+		self.position_button2 = Button(text=' ', pos=(578,95), size=(100, 75)) 
 		self.position_button2.bind(on_release=self.position_button2_callback)
 		
-		self.position_button3 = Button(text=' ', pos=(578,240), size=(100, 75)) 
+		self.position_button3 = Button(text=' ', pos=(578,168), size=(100, 75)) 
 		self.position_button3.bind(on_release=self.position_button3_callback)
 		
-		self.position_button4 = Button(text='', pos=(578,313), size=(100, 75)) 
+		self.position_button4 = Button(text='', pos=(578,240), size=(100, 75)) 
 		self.position_button4.bind(on_release=self.position_button4_callback)
 		
-		self.position_button5 = Button(text=' ', pos=(578,28), size=(100, 69)) 
+		self.position_button5 = Button(text=' ', pos=(578,313), size=(100, 69)) 
 		self.position_button5.bind(on_release=self.position_button5_callback)
 		
 		self.loop_button1 = Button(text= self.loop[0], pos=(750, 430), size=(50, 50))
@@ -110,10 +113,10 @@ class MyPaintApp(App):
 		parent.add_widget(self.position_button3)
 		parent.add_widget(self.position_button4)
 		parent.add_widget(self.position_button5)
-		parent.add_widget(self.loop_button1)
-		parent.add_widget(self.pick_drop_button1)
-		parent.add_widget(self.send_button1)
-		parent.add_widget(self.spy_position)
+		#parent.add_widget(self.loop_button1)
+		#parent.add_widget(self.pick_drop_button1)
+		#parent.add_widget(self.send_button1)
+		#parent.add_widget(self.spy_position)
 		parent.add_widget(self.shot_height_button)
 		parent.add_widget(self.return_button1)
 		parent.add_widget(self.return_button2)
@@ -128,19 +131,24 @@ class MyPaintApp(App):
 			if self.button_index[index] > 7:
 				self.button_index[index] = 0
 			obj.text=self.defenses[self.button_index[index]]
-
+			
+	
 	def defense_button2_callback(self, obj):
 		self.defense_callback(obj, 0)
-		
+		self.port.write("defense2 "+obj.text +":")
+
 	def defense_button3_callback(self, obj):
-		self.defense_callback(obj, 1)
-		
+		self.defense_callback(obj, 1)	
+		self.port.write("defense3 "+obj.text +":")
+
 	def defense_button4_callback(self, obj):
 		self.defense_callback(obj, 2)
+		self.port.write("defense4 "+obj.text +":")
 		
 	def defense_button5_callback(self, obj):
-		self.defense_callback(obj, 3)
-			
+		self.defense_callback(obj, 3)	
+		self.port.write("defense5 "+obj.text +":")
+
 	def config_button_callback(self, obj):
 		if self.configmode == True:
 			self.configmode = False 
@@ -153,9 +161,11 @@ class MyPaintApp(App):
 	def robot_start_position_callback(self, obj, index):
 		if self.configmode == True:
 			self.position_index[index] = self.position_index[index] + 1
+			self.port.write("position " + str(index + 1) + ":")
 			if self.position_index[index] > 1:
 				self.position_index[index] = 0
-			obj.text=self.robot[self.position_index[index]]
+				self.port.write("no_robot:")
+			obj.text=self.robot[self.position_index[index]] 
 			if self.position_index[index] == 1 and obj != self.last_robot_button:
 				if self.last_robot_button is not None:
 					self.last_robot_button.text = self.robot[0]
@@ -163,8 +173,8 @@ class MyPaintApp(App):
 				self.last_robot_button = obj
 			
 	def position_button1_callback(self, obj):
-		self.robot_start_position_callback(obj, 0) 
-			
+		self.robot_start_position_callback(obj, 0)
+
 	def position_button2_callback(self, obj):
 		self.robot_start_position_callback(obj, 1)
 		
@@ -202,69 +212,60 @@ class MyPaintApp(App):
 			self.high_low_buttonindex = self.high_low_buttonindex + 1
 			if self.high_low_buttonindex > 1:
 				self.high_low_buttonindex = 0
+				self.port.write("shoot true:")
+			else:	
+				self.port.write("shoot false:") 
 			obj.text=self.high_low_index[self.high_low_buttonindex]
+			 
 			
 	def send_button1_callback(self, obj):
+		pass
 		
-		if self.configmode == True:
+	'''	if self.configmode == True:
 			abvr = self.abvr_defenses[self.button_index[3]]
 			print (abvr)
 			for c in list (abvr):
-				joystick_protocol.send(c)
+				pass
+				
+				
 			abvr = self.abvr_defenses[self.button_index[2]]
 			print (abvr)
 			for c in list (abvr):
-				joystick_protocol.send(c)
+				pass			
 			abvr = self.abvr_defenses[self.button_index[1]]
 			print (abvr)
 			for c in list (abvr):
-				joystick_protocol.send(c)
+				pass
+				
 			abvr = self.abvr_defenses[self.button_index[0]]
 			print (abvr)
 			for c in list (abvr):
-				joystick_protocol.send(c) 
+				pass
+				
 			
 
 			if self.position_index[0] == 1:
-				joystick_protocol.send('r')
-				joystick_protocol.send('1')
-				joystick_protocol.send('0')
+				
+				
+				
 			if self.position_index[1] == 1:
-				joystick_protocol.send('r')
-				joystick_protocol.send('2')
-				joystick_protocol.send('0')
+				
 			if self.position_index[2] == 1:
-				joystick_protocol.send('r')
-				joystick_protocol.send('3')
-				joystick_protocol.send('0')
+				
 			if self.position_index[3] == 1:
-				joystick_protocol.send('r')
-				joystick_protocol.send('4')
-				joystick_protocol.send('0')
+				
 			if self.position_index[4] == 1:
-				joystick_protocol.send('r')
-				joystick_protocol.send('5')
-				joystick_protocol.send('0') 
+				 
 			if self.position_index2[0] == 1:
-				joystick_protocol.send('t')
-				joystick_protocol.send('1')
-				joystick_protocol.send('0')
+				
 			if self.position_index2[1] == 1:
-				joystick_protocol.send('t')
-				joystick_protocol.send('2')
-				joystick_protocol.send('0')
+				
 			if self.position_index2[2] == 1:
-				joystick_protocol.send('t')
-				joystick_protocol.send('3')
-				joystick_protocol.send('0')
+				
 			if self.position_index2[3] == 1:
-				joystick_protocol.send('t')
-				joystick_protocol.send('4')
-				joystick_protocol.send('0')
-			if self.position_index[4] == 1:
-				joystick_protocol.send('t')
-				joystick_protocol.send('5')
-				joystick_protocol.send('0')
+				
+			if self.position_index[4] == 1: '''
+				
 	def robot_start_position_callback2(self, obj, index):
 		if self.configmode == True:
 			self.position_index2[index] = self.position_index2[index] + 1
@@ -310,5 +311,6 @@ class MyPaintApp(App):
 			self.position_index[1] = list_index		
 		if self.position_button1 is obj:
 			self.position_index[0] = list_index			
+
 if __name__ == '__main__':
 	MyPaintApp().run()
